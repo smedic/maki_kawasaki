e3bf70ad9fde08135e6ab623718b9b689e14899e
MarijaDjuric <marija.djuric@symphony.is>
2023-05-04 13:45:46 +0200

    Add OtpTextField
    Move to Design screen
    
    Show pin text fields

--- app/src/main/java/com/example/myapplication/screens/LoginContent.kt
+++ app/src/main/java/com/example/myapplication/screens/LoginContent.kt
@@ -17,6 +17,9 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -62,6 +65,7 @@ fun LoginContent(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
             Text(
                 modifier = Modifier.clickable {
                     viewModel.login()
